b8b43f7dd9cebb7a58c8321765a68ced3adc32d2
Mohanad Abu-Nayla <aljaboori@gmail.com>
2021-10-06 22:01:51 +0800

    Setting debug to true.

--- core/views.py
+++ core/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    html = "<html><body> Welcome,It is now %s.</body></html>" % now
     return HttpResponse(html)
